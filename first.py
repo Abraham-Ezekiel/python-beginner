65739311b0b5123c3778a797ed55057177d109cf
Abraham Ezekiel Akpan <abraham@pcdeab.abpc>
2024-06-26 16:40:00 +0100

    starting offensive python for cybersecurity

---
+++ first.py
@@ -0,0 +1,23 @@
+#first class on python
+
+#print() is use to display info to the sdo
+
+print(234565)
+print('heloo biggi')
+#name = input('enter ur name:  ')
+#variable, a  space used to store values or infosh
+first_name = 'Bughacker'
+
+print(first_name) 
+
+#print(name)
+
+my_school = 'aksu'
+my_age = 18
+my_country = 'Naija'
+print(my_country, my_age, my_school)
+
+#data types:strings, float, int, complex, list, dictionary, tuple, set, frozen set, boolean.
+print(f'The name of my country is {my_country} and  my  age is {my_age} i am a student of {my_school}')
+print('the name of my country is {} and my age is {} i am a student of {}'.format(my_country, my_age, my_school))
+print(f'{my_age}\n{my_school}\t{my_country}')
---
+++ new.py
@@ -0,0 +1,17 @@
+print('u re welcome')
+first_name = input('pls ur first name: ')
+second_name = input('ur second name: ')
+last_name = input('ur last name: ')
+phone_number = input('ur phone num: ')
+email = input('ur email: ')
+age = input('ur age: ')
+
+
+print('now ur form is ready')
+#print(f'ur 1st name is: {first_name}\nur 2nd name is: {second_name}\nur last name is:  {last_name}\nphone number: {phone_number}\nemail: {email}\nur age: {age}')
+print(f'''ur 1st name is: {first_name}
+ur 2nd name is: {second_name}
+ur last name is:  {last_name}
+phone number: {phone_number}
+email: {email}
+ur age: {age}''')
